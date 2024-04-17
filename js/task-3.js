44eabcca624fbdb6fdd12ea1ea9cdf84f4f30ae0
vitayusko <vitayusko@users.noreply.github.com>
2024-04-17 22:38:25 +0200

    18/04

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,4 @@
-const sortByDescendingFriendCount = (users) => users.sort((a, b) => b.friends.length - a.friends.length);
+const sortByDescendingFriendCount = (users) => users.toSorted((a, b) => b.friends.length - a.friends.length);
 
 
 
@@ -78,4 +78,5 @@ console.log(
 //     gender: "male"
 //   }
 // ]
+
 
